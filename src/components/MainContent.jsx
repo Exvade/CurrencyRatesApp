74e5f0bfd3a012bc58e809ccc09e485f5335b1b3
Deft Valian Exanova <deftvalian2411@gmail.com>
2024-11-02 20:27:56 +0700

    modified

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -5,6 +5,10 @@ export default function MainContent() {
     <div className="w-full flex justify-center items-center h-[100vh] bg-[url('/src/assets/image.jpg')] bg-cover bg-center">
       <div className="w-[50%]">
         <Table />
+        <div className="flex flex-col justify-center items-center mt-4 font-bold text-yellow-400">
+          <p>Rates are based from 1 USD</p>
+          <p>This Application uses API from <a href="https://currencyfreaks.com" className="underline">Currency Freaks</a></p>
+        </div>
       </div>
     </div>
   )
